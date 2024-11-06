0a65e846106e6b43b3a9c53ddea614cd5b9bfb11
umais2005 <umaismuhammad99@gmail.com>
2024-11-07 00:17:15 +0500

    Update streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -3,7 +3,7 @@ import streamlit as st
 pg = st.navigation(
     [
     st.Page("./chapter_streamlit.py", title="Chapter", url_path="chapter",default=True),
-    st.Page("./streamlit_ui.py", title="Simple", url_path="qna")
+    st.Page("./simpleqna.py", title="Simple", url_path="qna")
     ]
 )
 pg.run()
